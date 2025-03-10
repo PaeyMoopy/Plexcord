c55588a4a8ff4887a28d4233b198625447304cc1
PaeyMoopy <51347174+PaeyMoopy@users.noreply.github.com>
2025-03-10 11:59:30 -0300

    Add files via upload

--- src/bot/services/overseerrRequests.js
+++ src/bot/services/overseerrRequests.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { client } from '../bot.js';
+import { client } from '../index.js';
 import { getDiscordId } from './overseerr.js';
 import { EmbedBuilder } from 'discord.js';
 
